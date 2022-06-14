d7ab519ca9055e55db35310f8c2e64547b44696a
Memo10100 <69870150+Memo10100@users.noreply.github.com>
2022-06-13 21:14:13 -0300

    first commit

---
+++ src/main/java/Application/Application.java
@@ -0,0 +1,17 @@
+
+package Application;
+
+import views.PrincipalView;
+
+
+public class Application {
+
+
+    public static void main(String[] args) {
+      
+        PrincipalView principal = new PrincipalView();
+        principal.setVisible(true);
+    }
+    
+}
+
